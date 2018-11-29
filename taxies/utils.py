5b3b1395f17b271e06d9610a5b9d2a9da3e75b2d
davidealbanese <davide.albanese@gmail.com>
2018-11-29 14:02:41 +0100

    ordered protein_names

--- taxies/utils.py
+++ taxies/utils.py
@@ -8,13 +8,13 @@ import dendropy
 
 def read_protein_names(input_fn):
         
-    protein_names = set()
+    protein_names = []
     with open(input_fn, 'r') as input_handle:
         for row in csv.reader(input_handle, delimiter='\t'):
             if len(row) > 0:
-                protein_names.add(row[0])
+                protein_names.append(row[0])
                 
-    return list(protein_names)
+    return protein_names
 
 
 class TaxReader:
